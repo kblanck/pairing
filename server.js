f2bf449a823a4f0c65ddd783091b0cb37e02a910
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-01-13 15:46:07 -0500

    serverjs

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 
 
 // CONFIGURATION
 const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+// MIDDLEWARE
+app.use(express.json()) //use .json(), not .urlencoded()
+app.use(express.static('public'))
 
 const instrumentsController = require('./controllers/instruments_controller.js')
 app.use('/instruments', instrumentsController)
 
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
 
-
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello Kim')
-// })
-
-
-
-
-
-
-
-
+// Error / success
+mongoose.connection.on('error', err =>
+  console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+)
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI)
+)
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 
 
